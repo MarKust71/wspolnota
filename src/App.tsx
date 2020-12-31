615de6843de5cfd7947f54d83dbd0d37454acb04
Marek Kustosz <markust71@hotmail.com>
2020-12-31 01:03:54 +0100

    commit#4

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ApartmentsContainer } from './components/ApartmentsContainer';
+
+function App() {
+    return <ApartmentsContainer />;
+}
+
+export default App;
